ee3c4d99e8192d2f2b4f4ca30f48a8f04687a007
DAN35RUS <67592638+DAN35RUS@users.noreply.github.com>
2020-08-07 13:46:59 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var instance = new vidbg('.video', {
-    mp4: "/video/world.mp4",
-    webm: "/video/world.webm",
-    poster: "/img/poster.jpg",
+    mp4: "./video/world.mp4",
+    webm: "./video/world.webm",
+    poster: "./img/poster.jpg",
     overlay: false,
 });
 
